4842e9e62c2d629a2902628185eae7a352e4d0b6
sarvesh gupta <sarveshgupta9173@gmail.com>
2023-05-16 11:19:17 +0530

    colum-grid complete

--- Block/Product/grid.php
+++ Block/Product/grid.php
@@ -1,12 +1,40 @@
 <?php 
 
 
-class Block_Product_Grid extends Block_Core_Template
+class Block_Product_Grid extends Block_Core_Grid
 {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->setTemplate('Product/grid.phtml');
+		// $this->setTemplate('Product/grid.phtml');
+		$this->setTitle('Manage Product');
+	}
+
+	public function prepareButtons()
+	{
+		$this->addButton('product_id',['title'=>'ADD PRODUCT','url'=>$this->getUrl('add')]);
+		return parent::prepareButtons();
+	}
+
+	public function prepareActions()
+	{
+		$this->addAction('Edit',['title'=>'EDIT','method'=>'getEditUrl']);
+		$this->addAction('Delete',['title'=>'DELETE','method'=>'getDeleteUrl']);
+		return parent::prepareActions();
+
+		
+	}
+
+	public function prepareColumns()
+	{
+		$this->addColumn('product_id',['title'=>'Product Id']);
+		$this->addColumn('name',['title'=>'Name']);
+		$this->addColumn('cost',['title'=>'Cost']);
+		$this->addColumn('price',['title'=>'Price']);
+		$this->addColumn('sku',['title'=>'SKU']);
+		$this->addColumn('quantity',['title'=>'Quantity']);
+		$this->addColumn('created_at',['title'=>'Created At']);
+		$this->addColumn('updated_at',['title'=>'Updated At']);	
 	}
 
 	public function getCollection()
